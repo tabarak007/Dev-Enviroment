3d26f34ef9724fd3fd3b59404e7dd9d4de2282ef
hamidulhussain13-ops <hamidul.hussain13@gmail.com>
2026-07-17 04:08:02 +0530

    backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
 terraform {
+   backend "azurerm" {
+    resource_group_name  = "rg-backend"
+    storage_account_name = "tabarak007"
+    container_name       = "hussain007"
+    key                  = "module.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
